72a13c61472982966cd45333780e56d907f066c6
Benjamin Parker <benjamin@needstyping.com>
2018-06-22 16:22:34 +0100

    privacy

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -7,14 +7,14 @@ object Checkout {
 
   def totalCost(items: List[String]): Int = cost(items) - discount(items)
 
-  def cost(items: List[String]): Int = items.foldLeft(0)(_ + prices(_))
+  private def cost(items: List[String]): Int = items.foldLeft(0)(_ + prices(_))
 
-  def discount(items: List[String]): Int = {
+  private def discount(items: List[String]): Int = {
     val (apples, oranges) = items.partition(_ == "Apple")
     appleDiscount(apples) + orangeDiscount(oranges)
   }
 
-  def appleDiscount(apples: List[String]): Int = apples.length / 2  * prices("Apple")
+  private def appleDiscount(apples: List[String]): Int = apples.length / 2 * prices("Apple")
 
-  def orangeDiscount(oranges: List[String]): Int = oranges.length / 3  * prices("Orange")
+  private def orangeDiscount(oranges: List[String]): Int = oranges.length / 3 * prices("Orange")
 }
